945c7714d98d1e2a73bfab57da228e8158ce2c4b
Anvar Kunanbayev <kunanbaevanvar@gmail.com>
2022-01-08 20:10:35 +0800

    custom polynomial code

--- utils/poly1d.cpp
+++ utils/poly1d.cpp
@@ -1,7 +1,7 @@
-// This code was borrowed from NumCPP project. at https://github.com/dpilger26/NumCpp
 #include <iostream>
 #include <vector>
 #include <cstdint>
+#include <cmath>
 
 class Poly1d
 {
@@ -14,7 +14,7 @@ class Poly1d
 
         //============================================================================
         // Method Description:
-        Poly1d(const std::vector<double>& inValues)
+        void init (const std::vector<double>& inValues)
         {
             // std::vector<double> coefficients_;
             for (auto value : inValues)
@@ -22,51 +22,59 @@ class Poly1d
                 coefficients_.push_back(value);
             }
         }
+            
+        std::vector<double> evaluate (std::vector<double> inValues)
+        {
+            std::vector<double> result;
+            
+            for (int i = 0; i < inValues.size(); i++)
+            {
+                int polyValue = 0;
+                int power = inValues.size() - 1;
+                for (auto coeff : coefficients_)
+                {
+                    polyValue += coeff * pow(inValues[i], power);
+                    power--;
+                }
+                result.push_back(polyValue);
+            }
+            
+            return result;
+        }
+            
+        
 };
 
 //============================================================================
 // Method Description:
-std::vector<double> Poly1d :: fit(const std::vector<double>& xValues, const std::vector<double>& yValues, uint8_t polyOrder)
-{
-    int numMeasurements = xValues.size();
+//std::vector<double> Poly1d :: fit(const std::vector<double>& xValues, const std::vector<double>& yValues, uint8_t polyOrder)
+//{
+//  int numMeasurements = xValues.size();
+//
+//  NdArray<double> a(numMeasusrements, polyOrder + 1);
+//  for (uint32 measIdx = 0; measIdx < numMeasurements; ++measIdx)
+//  {
+//      const auto xDouble = static_cast<double>(xValues[measIdx]);
+//      for (uint8 order = 0; order < a.numCols(); ++order)
+//      {
+//          a(measIdx, order) = utils::power(xDouble, order);
+//      }
+//  }
+//
+//  NdArray<double> aInv;
+//  if (a.issquare())
+//  {
+//      aInv = linalg::inv(a);
+//  }
+//  else
+//  {
+//      // psuedo-inverse
+//      auto aT = a.transpose();
+//      auto aTaInv = linalg::inv(aT.dot(a));
+//      aInv = aTaInv.dot(aT);
+//  }
+//  
+//  auto x = aInv.dot(yValues.template astype<double>());
+//  return Poly1d<double>(x);
+//}
 
-    NdArray<double> a(numMeasusrements, polyOrder + 1);
-    for (uint32 measIdx = 0; measIdx < numMeasurements; ++measIdx)
-    {
-        const auto xDouble = static_cast<double>(xValues[measIdx]);
-        for (uint8 order = 0; order < a.numCols(); ++order)
-        {
-            a(measIdx, order) = utils::power(xDouble, order);
-        }
-    }
-
-    NdArray<double> aInv;
-    if (a.issquare())
-    {
-        aInv = linalg::inv(a);
-    }
-    else
-    {
-        // psuedo-inverse
-        auto aT = a.transpose();
-        auto aTaInv = linalg::inv(aT.dot(a));
-        aInv = aTaInv.dot(aT);
-    }
-    
-    auto x = aInv.dot(yValues.template astype<double>());
-    return Poly1d<double>(x);
-}
-
-//============================================================================
-// Method Description:
-std::vector<double> Poly1d :: operator()(std::vector<double> inValue)
-{
-    int polyValue = 0;
-    uint8_t power = 0;
-    for (auto& coefficient : (*this)coefficients_)
-    {
-        polyValue += coefficient * utils::power(inValue, power++);
-    }
-
-    return polyValue;
-}
