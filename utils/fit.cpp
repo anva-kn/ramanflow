ba420cc9e02e143353ecb7e2fc937307a66e9d0a
anva-kn <kunanbaevanvar@gmail.com>
2022-01-08 12:09:32 +0000

    some code for polynomial

--- utils/fit.cpp
+++ utils/fit.cpp
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <algorithm>
 #include <iterator>
-#include <Python.h>
+// #include <Python.h>
 #include <pybind11.h>
 #include <stl.h>
 #include <operators.h>
@@ -15,14 +15,22 @@ namespace py = pybind11;
 
 using namespace std;
 
-std::vector<double> Fit :: PolyBiquadratic(std::vector<double> x_data, double beta[])
+void Fit :: PolyBiquadratic(std::vector<double> x_data, double *beta, int order, std::vector<double> eval_pts)
 {
     int poly_size = int(x_data.size());
+    int ordr = order;
+    int exp[poly_size];
+    double x_data_arr[x_data.size()];
+    for (std::size_t i = 0; i < poly_size; i++)
+    {
+        exp[i] = poly_size - i;
+        x_data_arr[i] = x_data[i];
+    }
 //  polynomial_print(1, poly_size, beta);
-    double c[11];
+//    double c[11];
     double c1[6] = { 7.0, - 5.0, 9.0, 11.0, 0.0, - 13.0 };
     double c2[5] = { 2.0, 3.0, -8.0, 4.0, 9.0 };
-    int m = 3;
+    int m = 1;
     int e[11];
     int e1[6] = { 1, 2, 4, 5, 12, 33 };
     int e2[5] = { 1, 3, 4, 30, 33 };
@@ -33,8 +41,61 @@ std::vector<double> Fit :: PolyBiquadratic(std::vector<double> x_data, double be
     string title1 = "  P1(X) =";
     string title2 = "  P2(X) =";
     cout << "\n";
+    cout << "ordr: " << ordr << endl;
+    cout << "poly_size: " << poly_size << endl;
+    cout << "x_data: ";
+    for (auto i : x_data)
+    {
+        cout << x_data[i] << " ";
+    }
+    cout << "exp: ";
+    for (int i = 0; i < sizeof(exp) / sizeof(exp[0]); i++)
+    {
+        cout << exp[i] << " ";
+    }
+    cout << endl;
+    cout << "m: " << m << endl;
+    cout << "o1: " << o1 << endl;
+    cout << "c1: ";
+    for (int i = 0; i < sizeof(c1) / sizeof(c1[0]); i++)
+    {
+        cout << c1[i] << " ";
+    }
+    cout << "e1: ";
+    for (int i = 0; i < sizeof(e1) / sizeof(e1[0]); i++)
+    {
+        cout << e1[i] << " ";
+    }
+    double *c, *p;
+    double points[eval_pts.size()];
+    double x[3*2] = {
+                    1.0, 2.0, 3.0,
+                    -2.0, 4.0, 1.0 };
+    polynomial_print(ordr, poly_size, x_data_arr, exp, title1);
+    c = polynomial_value(ordr, poly_size, x_data_arr, exp, 1, points);
+    cout << endl;
     polynomial_print ( m, o1, c1, e1, title1 );
-//  p = np.poly1d(beta)
+    p = polynomial_value ( m, o, c, e, 2, x );
+
+    cout << "\n";
+    for (int j = 0; j < 2; j++ )
+    {
+        cout << "  P(" << x[0+j*m]
+        << "," << x[1+j*m]
+        << "," << x[2+j*m]
+        << ") = " << p[j] << "\n";
+    }
+
+    delete [] p;
+    //  p = np.poly1d(beta)
+    cout << "\n";
+    cout << "c: "
+    for (int j = 0; j < 4; j++ )
+    {
+        cout << c[j] << " ";
+    }
+
+    delete [] c;
 //  return p(x_data)
 }
 
@@ -71,7 +132,7 @@ std::vector<double> Fit :: InitPseudoVoight(std::vector<double> &x_data, std::ve
     std::vector<double>::iterator max_element = std::max_element(y_data.begin(), y_data.end());
     int argmaxVal = std::distance(y_data.begin(), max_element);
     beta_init_lor[0] = *std::max_element(y_data.begin(), y_data.end());
-    beta_init_lor[1] = x_data[*max_element];
+    beta_init_lor[1] = x_data[argmaxVal];
     beta_init_lor[2] = (x_data.end() - x_data.begin()) / 16.0;
     beta_init_lor[3] = 0.0;
     return beta_init_lor;
